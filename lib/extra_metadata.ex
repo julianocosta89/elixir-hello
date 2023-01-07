0a4e6e009eb1f85149a3db44bdad164010c03a35
Juliano Costa <julianocosta89@outlook.com>
2023-01-07 02:10:41 +0100

    refactor

--- lib/extra_metadata.ex
+++ lib/extra_metadata.ex
@@ -3,36 +3,23 @@ defmodule ExtraMetadata do
   @behaviour :otel_resource_detector
 
   def get_resource(_) do
-    lines =
-      case File.read("extrametadata.properties") do
-        {:ok, contents} ->
-          String.split(contents, "\n")
-        {:error, _reason} ->
-          []
-      end
+    lines = read_file("extrametadata.properties") |> unwrap_lines
+    file_path = read_file("hiddenpath.properties") |> unwrap_lines
+    lines2 = read_file(file_path) |> unwrap_lines
+    attributes = get_attributes(Enum.concat(lines, lines2))
+    :otel_resource.create(attributes)
+  end
 
-    filePath =
-      case File.read("hiddenpath.properties") do
-        {:ok, contents} ->
-          String.split(contents, "\n")
-        {:error, _reason} ->
-          []
-      end
+  defp unwrap_lines({:ok, lines}), do: lines
+  defp unwrap_lines({:error, _}), do: []
 
-    lines2 =
-      case File.read(filePath) do
-        {:ok, contents} ->
-          String.split(contents, "\n")
-        {:error, _reason} ->
-          []
-      end
-
-    attributes = get_attributes(lines)
-    attributes2 = get_attributes(lines2)
-
-    resource1 = :otel_resource.create(attributes)
-    resource2 = :otel_resource.create(attributes2)
-    :otel_resource.merge(resource1, resource2)
+  defp read_file(file_name) do
+    try do
+      {:ok, String.split(File.read!(file_name), "\n")}
+    rescue
+      File.Error ->
+        {:error, "File does not exist, safe to continue"}
+    end
   end
 
   defp get_attributes(lines) do
